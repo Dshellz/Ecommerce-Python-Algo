be468bb3727aad9f4d27f7304059043538902c4f
Dshellz <davidsanchez38550@gmail.com>
2025-01-08 17:55:11 +0100

    Menu interface

--- Menu_Interactif.py
+++ Menu_Interactif.py
@@ -8,213 +8,265 @@ from tri_b import produits_trie
 from logging_log import creer_compte, connection, ajout_prod, mdp_compromis
 import pandas as pd
 from commercants import filtre_nom
-with open('produits.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
+import tkinter as tk
+from tkinter import messagebox
 
 
-def afficher_menu(): # Affichage menu avec les options 
-    print("\n=== MENU ===")
-    print("1| Afficher les produits")
-    print("2| Ajouter un nouveau produit")
-    print("3| Supprimer un produit")
-    print("4| Rechercher un produit")
-    print("5| Trier les produits par nom")
-    print("6| Trier les produits par prix")
-    print("7| Rechercher un commerçants")
-    print("8| Modifier votre mot de passe")
-    print("9| Quitter")
-
-def afficher_produits(): # affichier les produits du fichier.csv
- with open('produits.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        print("\n=== LISTE DES PRODUITS ===")
-        for row in reader:
-            print(f"Commerçant : {row['nom']} : {row['produit']} {row['quantite']} en stock Prix = {row['prix']} €")
-
-def ajouter_produit(nom, produit, quantite, prix): # Ajout d'un produit 
-    with open('produits.csv', mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([nom ,produit, quantite, prix])
-        ajout_prod(nom, produit, quantite, prix) # Pour les logs
-
-def supprimer_produit(): # Suppression d'un produit
-    produit_a_supprimer = input("Entrez le nom du produit à supprimer : ")
-    produits_restants = []
-    produit_trouve = False
-    
-    with open('produits.csv', newline='') as csvfile: # Cherche si le produit demander existe
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if row['produit'] != produit_a_supprimer:
-                produits_restants.append(row)
-            else:
-                produit_trouve = True
-    if produit_trouve:
-        print(f"Le produit '{produit_a_supprimer}' a été supprimé.")
-    else:
-        print(f"Produit '{produit_a_supprimer}' non trouvé.")
-        return
-    with open('produits.csv', 'w', newline='') as csvfile: # Réecriture du fichier produits.csv avec les produits restants
-        fieldnames = ['produit', 'quantite', 'prix']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for produit in produits_restants:
-            writer.writerow(produit)
-
-def quicksort_prix():
-    print(df_sorted)
-
-def recherche_produit(sproduit): # Recherche ligne par ligne
-    with open("produits.csv", "r", newline='', encoding='utf-8') as fichier:
-        donnee = list(csv.reader(fichier, delimiter=";"))
-        for ligne in donnee:
-            if sproduit.lower() in ligne[0].lower(): 
-                print(f"Produit trouvé : {ligne}")
-                break
+is_logged_in = False
+logged_user = None
 
 def genere_salage(lenght=16):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=lenght))
 
-def register():
-    with open("users.csv", mode="a", encoding='utf-8', newline="") as f:
-        writer = csv.writer(f, delimiter=",")
-        email = input("Entrer un email : ")
-        password = input("Entrer votre mot de passe : ")
-        password2 = input("Confirmer votre mot de passe : ")
-        if password == password2:
-            salt = genere_salage()
-            # password_salage = password + salt
-            pw_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-            prefix = pw_hash[:5]
-            suffix = pw_hash[5:] 
+def register(email_entry, password_entry, password2_entry, result_label):
+    email = email_entry.get()
+    password = password_entry.get()
+    password2 = password2_entry.get()
 
-            url = f"https://api.pwnedpasswords.com/range/{prefix}"
-            response = requests.get(url)
-            if response.status_code != 200:
-                raise RuntimeError(f"Error: {response.status_code}")
-            
-            found = False
-            hashes = (line.split(':') for line in response.text.splitlines())
-            for returned_suffix, count in hashes:
-                if returned_suffix == suffix:
-                    print(f"Mot de passe trop peu sécurisé ! Il à été compromis {count} fois.\nVeuillez mettre un mot de passe plus sécurisé.")
-                    found = True
-                    mdp_compromis() # Pour les logs 
-                    return register()
-            if not found :
-                print("Mot de passe sécurisé (aucunes traces de fuites de données de ce mot de passe).")
-                writer.writerow([email,pw_hash,salt])
-                creer_compte(email) # Pour les logs
-                print("Votre compte a été créé avec succès ! ")              
-        else:
-            print("Les mots de passes ne correspondent pas. Veuillez réessayer")
-            return register()
+    if password == password2:
+        salt = genere_salage()
+        pw_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+        prefix = pw_hash[:5]
+        suffix = pw_hash[5:]
 
+        url = f"https://api.pwnedpasswords.com/range/{prefix}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise RuntimeError(f"Error: {response.status_code}")
 
-def login():
+        found = False
+        hashes = (line.split(':') for line in response.text.splitlines())
+        for returned_suffix, count in hashes:
+            if returned_suffix == suffix:
+                result_label.config(text=f"Mot de passe trop peu sécurisé ! Il a été compromis {count} fois.\nVeuillez mettre un mot de passe plus sécurisé.")
+                mdp_compromis()
+                return
+
+        with open("users.csv", mode="a", encoding='utf-8', newline="") as f:
+            writer = csv.writer(f, delimiter=",")
+            writer.writerow([email, pw_hash, salt])
+            creer_compte(email)
+
+        result_label.config(text="Votre compte a été créé avec succès !")
+        messagebox.showinfo("Succès", "Votre compte a été créé avec succès !")
+
+    else:
+        result_label.config(text="Les mots de passes ne correspondent pas. Veuillez réessayer.")
+        messagebox.showerror("Erreur", "Les mots de passe ne correspondent pas. Veuillez réessayer.")
+
+def login(email_entry, password_entry, result_label):
+
+    global is_logged_in, logged_user
+
+    email = email_entry.get()
+    password = password_entry.get()
+
     with open("users.csv", mode="r", encoding='utf-8') as file:
         reader = csv.reader(file)
-        email = input("Entrez votre email : ")
-        password = input("Entrez votre mot de passe : ")
         for row in reader:
             reg_name = row[0]
             reg_pass = row[1]
-            # salt_stocker = row[2] 
-            # password_salage = password + salt_stocker
             pw_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
             if pw_hash == reg_pass and email == reg_name:
-                connection(email) # Pour les logs
-                print(f"\nBienvenue {email}")
-                return True
-    print("Les informations que vous avez rentrez sont incorrectes !")
-    return False
-def main():
-    choix_initial = input("1 | Se connecter\n2 | Créer un compte\nChoisissez une option : ")
+                logged_user = email
+                is_logged_in = True
+                result_label.config(text=f"Bienvenue {email}")
+                messagebox.showinfo("Connexion réussie", f"Bienvenue {email}")
+                return
 
-    if choix_initial == "1":
-        if login():
-            pass
-        else:
-            print("Impossible de se connecter. Réessayez.")
-            main()
-    elif choix_initial == "2":
-        register()
-        main()
+    result_label.config(text="Les informations que vous avez rentrées sont incorrectes !")
+    messagebox.showerror("Erreur", "Les informations que vous avez rentrées sont incorrectes !")
+
+
+
+def show_main_menu():
+    main_menu_window = tk.Toplevel()
+    main_menu_window.title("Menu Principal")
+    main_menu_window.geometry("350x120")
+
+    def afficher_produits():
+        show_prod = tk.Tk()
+        show_prod.title("Produits")
+        show_prod.geometry("450x250")
+
+        text_area = tk.Text(show_prod, height=15, width=50)
+        text_area.pack(padx=10, pady=10)
+
+        with open('produits.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            produits = []
+            for row in reader:
+                produits.append (f" {row['nom']}: {row['produit']}: {row['quantite']} en stock, Prix = {row['prix']} €")
+        
+        text_area.delete(1.0, tk.END)
+        text_area.insert(tk.END, "\n".join(produits))
+
+
+            
+    def ajouter_produit():
+        def save_product():
+            nom = nom_entry.get()
+            produit = produit_entry.get()
+            quantite = quantite_entry.get()
+            prix = prix_entry.get()
+            with open('produits.csv', mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([nom, produit, quantite, prix])
+            messagebox.showinfo("Succès", "Produit ajouté avec succès.")
+            ajouter_window.destroy()
+
+        ajouter_window = tk.Toplevel(main_menu_window)
+        ajouter_window.title("Ajouter un produit")
+        
+        tk.Label(ajouter_window, text="Nom du commerçant:").pack()
+        nom_entry = tk.Entry(ajouter_window)
+        nom_entry.pack()
+
+        tk.Label(ajouter_window, text="Nom du produit:").pack()
+        produit_entry = tk.Entry(ajouter_window)
+        produit_entry.pack()
+
+        tk.Label(ajouter_window, text="Quantité:").pack()
+        quantite_entry = tk.Entry(ajouter_window)
+        quantite_entry.pack()
+
+        tk.Label(ajouter_window, text="Prix:").pack()
+        prix_entry = tk.Entry(ajouter_window)
+        prix_entry.pack()
+
+        tk.Button(ajouter_window, text="Ajouter", command=save_product).pack()
+
+    tk.Button(main_menu_window, text="Afficher les produits", command=afficher_produits).pack()
+
+    tk.Button(main_menu_window, text="Ajouter un produit", command=ajouter_produit).pack()
+    # tk.Button(main_menu_window) text="Supprimer un produit", command=supprimer_produit).pack()
+    tk.Button(main_menu_window, text="Quitter", command=main_menu_window.destroy).pack()
+
+# def supprimer_produit(): # Suppression d'un produit
+#     produit_a_supprimer = input("Entrez le nom du produit à supprimer : ")
+#     produits_restants = []
+#     produit_trouve = False
+    
+#     with open('produits.csv', newline='') as csvfile: # Cherche si le produit demander existe
+#         reader = csv.DictReader(csvfile)
+#         for row in reader:
+#             if row['produit'] != produit_a_supprimer:
+#                 produits_restants.append(row)
+#             else:
+#                 produit_trouve = True
+#     if produit_trouve:
+#         print(f"Le produit '{produit_a_supprimer}' a été supprimé.")
+#     else:
+#         print(f"Produit '{produit_a_supprimer}' non trouvé.")
+#         return
+#     with open('produits.csv', 'w', newline='') as csvfile: # Réecriture du fichier produits.csv avec les produits restants
+#         fieldnames = ['produit', 'quantite', 'prix']
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#         writer.writeheader()
+#         for produit in produits_restants:
+#             writer.writerow(produit)
+
+# def quicksort_prix():
+#     print(df_sorted)
+
+# def recherche_produit(sproduit): # Recherche ligne par ligne
+#     with open("produits.csv", "r", newline='', encoding='utf-8') as fichier:
+#         donnee = list(csv.reader(fichier, delimiter=";"))
+#         for ligne in donnee:
+#             if sproduit.lower() in ligne[0].lower(): 
+#                 print(f"Produit trouvé : {ligne}")
+#                 break
+
+# def change_pw():
+#     email = input("Entrez votre email : ")
+#     old_password = input("Entrez votre ancien mot de passe : ")
+
+#     with open("users.csv", mode="r", encoding='utf-8') as file:
+#         rows = list(csv.reader(file))
+#     for row in rows:
+#         reg_name = row[0]
+#         reg_pass = row[1]
+        
+#         if email == reg_name:
+#             pw_hash = hashlib.sha1(old_password.encode('utf-8')).hexdigest().upper()
+#             if pw_hash == reg_pass:
+#                 print("Ancien mot de passe validé.")
+                
+#                 new_password = input("Entrez votre nouveau mot de passe : ")
+#                 new_password2 = input("Confirmez votre nouveau mot de passe : ")
+                
+#                 if new_password == new_password2:
+#                     salt = genere_salage()
+#                     pw_hash = hashlib.sha1(new_password.encode('utf-8')).hexdigest().upper()
+                    
+#                     with open("users.csv", mode="w", encoding='utf-8', newline="") as file:
+#                         writer = csv.writer(file, delimiter=",")
+#                         for r in rows:
+#                             if r[0] == email:
+#                                 writer.writerow([r[0], pw_hash, salt])
+#                             else:
+#                                 writer.writerow(r)
+                    
+#                     print("Votre mot de passe a été modifié avec succès !")
+#                     return True
+#                 else:
+#                     print("Les mots de passe ne correspondent pas. Veuillez réessayer.")
+#                     return change_pw()
+#     print("Email ou mot de passe incorrect.")
+#     return False
+
+def access_login(label):
+    if is_logged_in:
+        show_main_menu()
     else:
-        print("Choix invalide. Entrer 1 pour se connecter ou 2 pour créer un compte.")
-        main()
+        label.config(text="Accès refusé")
+        messagebox.showwarning("Accès interdit. Réessayer.")
+        
 
+def main_window():
+    window = tk.Tk()
+    window.title("Authenfication")
+
+    label_email = tk.Label(window, text="Email:")
+    label_email.grid(row=0, column=0)
+
+    email_entry = tk.Entry(window)
+    email_entry.grid(row=0, column=1)
+
+    label_password = tk.Label(window, text="Mot de passe:")
+    label_password.grid(row=1, column=0)
+
+    password_entry = tk.Entry(window, show="*")
+    password_entry.grid(row=1, column=1)
+
+    label_password2 = tk.Label(window, text="Confirmer le mot de passe:")
+    label_password2.grid(row=2, column=0)
+
+    password2_entry = tk.Entry(window, show="*")
+    password2_entry.grid(row=2, column=1)
+
+    result_label = tk.Label(window, text="")
+    result_label.grid(row=4, column=1)
+
+    def show_register_window():
+        register(email_entry, password_entry, password2_entry, result_label)
+
+    def show_login_window():
+        login(email_entry, password_entry, result_label)
+
+    button_register = tk.Button(window, text="Créer un compte", command=show_register_window)
+    button_register.grid(row=3, column=0)
+
+    button_login = tk.Button(window, text="Se connecter", command=show_login_window)
+    button_login.grid(row=3, column=1)
+
+    button_access = tk.Button(window, text="Accéder à la suite", command=access_login)
+    button_access.grid(row=5, column=0, columnspan=2)
+
+    window.mainloop()
 
 if __name__ == "__main__":
-    main()
+    main_window()
 
-def change_pw():
-    email = input("Entrez votre email : ")
-    old_password = input("Entrez votre ancien mot de passe : ")
-
-    with open("users.csv", mode="r", encoding='utf-8') as file:
-        rows = list(csv.reader(file))
-    for row in rows:
-        reg_name = row[0]
-        reg_pass = row[1]
-        
-        if email == reg_name:
-            pw_hash = hashlib.sha1(old_password.encode('utf-8')).hexdigest().upper()
-            if pw_hash == reg_pass:
-                print("Ancien mot de passe validé.")
-                
-                new_password = input("Entrez votre nouveau mot de passe : ")
-                new_password2 = input("Confirmez votre nouveau mot de passe : ")
-                
-                if new_password == new_password2:
-                    salt = genere_salage()
-                    pw_hash = hashlib.sha1(new_password.encode('utf-8')).hexdigest().upper()
-                    
-                    with open("users.csv", mode="w", encoding='utf-8', newline="") as file:
-                        writer = csv.writer(file, delimiter=",")
-                        for r in rows:
-                            if r[0] == email:
-                                writer.writerow([r[0], pw_hash, salt])
-                            else:
-                                writer.writerow(r)
-                    
-                    print("Votre mot de passe a été modifié avec succès !")
-                    return True
-                else:
-                    print("Les mots de passe ne correspondent pas. Veuillez réessayer.")
-                    return change_pw()
-    print("Email ou mot de passe incorrect.")
-    return False
-
-def menu_principal(): # Menu Principale
-    while True:
-        afficher_menu()
-        choix = input("Choisissez une option : ")
-        if choix == "1":
-            afficher_produits()
-        elif choix == "2":
-            nom = input("Entrer votre nom : ")
-            produit = input("Entrer un nom de produit : ")
-            quantite = int(input("Entrer la quantite : "))
-            prix = float(input("Entrer un prix : "))
-            ajouter_produit(nom, produit, quantite, prix)
-        elif choix == "3":
-            supprimer_produit()
-        elif choix == "4":
-            sproduit = input("Entrer le nom du produit que vous cherchez : ")
-            print("Si le produit n'apparait pas c'est qu'il n'existe pas.")
-            recherche_produit(sproduit)
-        elif choix == "5":
-            print(produits_trie)
-        elif choix == "6":
-            quicksort_prix()
-        elif choix =="7":
-            filtre_nom()
-        elif choix == "8":
-            change_pw()
-        elif choix == "9":
-            print("Au revoir !")
-            break
-        else:
-            print("Option invalide. Veuillez réessayer.")
-menu_principal()
+login_button = tk.Button(text="Accéder au menu principal", command=show_main_menu)
+login_button.pack(pady=20)
